7438d4975194779dceba22b10dbeab30fdc23cca
ZugastiKevin <bearded-developer@protonmail.com>
2025-04-15 16:40:27 +0200

    add: json and script

--- pages/events/scripts.js
+++ pages/events/scripts.js
@@ -1,7 +1,16 @@
-import events from "event.json";
-
 const eventTitle = document.querySelector(".event-title");
 const eventSubTitle = document.querySelector(".event-sub-title");
 const eventTexte = document.querySelector(".event-texte");
 const eventCardsContainer = document.querySelector(".event-cards-container");
 const eventButton = document.querySelector(".event-button");
+
+function startPage() {
+    const params = new URLSearchParams(document.location.search);
+    console.log(params.get("event"));
+    
+    //if (params.get("event") === "mariage") {
+        //eventTitle.textContent = events["mariage"][0];
+    //}
+};
+
+startPage();
