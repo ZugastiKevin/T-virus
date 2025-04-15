13cbad99e45669a0705ca273db8cea270884e453
ZugastiKevin <bearded-developer@protonmail.com>
2025-04-15 14:45:17 +0200

    add: json and script

---
+++ pages/events/scripts.js
@@ -0,0 +1,7 @@
+import events from "event.json";
+
+const eventTitle = document.querySelector(".event-title");
+const eventSubTitle = document.querySelector(".event-sub-title");
+const eventTexte = document.querySelector(".event-texte");
+const eventCardsContainer = document.querySelector(".event-cards-container");
+const eventButton = document.querySelector(".event-button");
