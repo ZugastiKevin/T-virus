4e58b2fd8fcf5f5d00b929a0c407ce1a698a0635
Marie Rivier <marie.rivier23@gmail.com>
2025-04-16 11:34:02 +0200

    modif dossier scripts

--- assets/scripts/devis.js
+++ assets/scripts/devis.js
@@ -1,25 +1,3 @@
-//script footer formulaire
-const form = document.querySelector('#devisform');
-
-form.addEventListener('submit', function(e) {
-e.preventDefault();
-
-form.reset();
-
-
-const message = document.createElement('p');
-message.textContent = "Devis envoyé !";
-message.style.color = '#727070';
-message.style.marginTop = '10px';
-
-const bouton = form.querySelector('#devisform .submit-btn');
-bouton.parentNode.appendChild(message)
-setTimeout(() => {
-    message.remove();
-  }, 3000);
-});
-
-
 //Script devis
 const formulaire = document.querySelector('#contactform');
 
