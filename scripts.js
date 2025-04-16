12ace3373fd5e622ecd288b79a683277cb9391b9
Norman BELAID <normanbelaid@gmail.com>
2025-04-16 11:44:39 +0100

    header 99%

--- scripts.js
+++ scripts.js
@@ -17,3 +17,32 @@ setTimeout(() => {
     message.remove();
   }, 3000);
 });
+
+
+// NAVBAR
+const wrapper = document.querySelector(".search-wrapper");
+const icon = wrapper.querySelector("i");
+let scrolled = false;
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  if (scrollY > 100 && !scrolled) {
+    scrolled = true;
+    wrapper.classList.add("compact");
+    wrapper.classList.remove("active");
+  } else if (scrollY <= 100 && scrolled) {
+    scrolled = false;
+    wrapper.style.animation = "slideUp 0.3s ease forwards";
+    setTimeout(() => {
+      wrapper.classList.remove("compact");
+      wrapper.style.animation = "";
+    }, 300);
+  }
+});
+
+icon.addEventListener("click", () => {
+  if (scrolled) {
+    wrapper.classList.toggle("active");
+  }
+});
